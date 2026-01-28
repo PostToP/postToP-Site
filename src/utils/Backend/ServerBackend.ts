764e3e00c7757b82124fece42711fedba59c70db
GitDevla <pataki.david.ferenc@gmail.com>
2026-01-28 17:39:09 +0100

    feat: design landing page

---
+++ src/utils/Backend/ServerBackend.ts
@@ -0,0 +1,22 @@
+import { User } from "@/context/AuthContext";
+import { Backend, type BackendResponse } from "./Backend";
+
+export interface ServerStats {
+    status: string
+    data: Data
+}
+
+export interface Data {
+    activeUsers: number
+    connectedUsers: number
+    musicVideos: string
+    musicArtists: string
+    totalListenedHours: number
+}
+
+export default class ServerBackend extends Backend {
+    static async getStats(): Promise<BackendResponse<ServerStats>> {
+        return ServerBackend.GET(
+            "/stats");
+    }
+}

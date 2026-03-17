be45c6d6b6b5d6154ce0dfc17f1f787fff784778
GitDevla <pataki.david.ferenc@gmail.com>
2026-03-17 19:46:44 +0100

    feat: add logo.svg and integrate into Navbar component

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import LogoSVG from "../Misc/LogoSvg";
 import UserDropdownMenu from "../Misc/UserDropdownMenu";
 
 export default function Navbar() {
     return (
         <nav className="w-full h-16 bg-primary flex items-center px-4">
-            <Link href="/">
+            <Link href="/" className="flex items-center">
+                {/* add logo.svg */}
+                <LogoSVG className="w-8 h-8 mr-2" />
                 <h1 className="text-lg font-bold">PostToP</h1>
             </Link>
             <div className="ml-auto">
